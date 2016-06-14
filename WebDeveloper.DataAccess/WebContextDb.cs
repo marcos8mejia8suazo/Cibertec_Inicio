2aeb7723fae93a9beb27073972526f067271323d
marcos8mejia8suazo <marcos88mejia@hotmail.com>
2016-06-13 22:29:04 -0500

    13062016:Creacion DbContext, configuracion de connections string, proyectos para Modelos y DataAccess.

---
+++ WebDeveloper.DataAccess/WebContextDb.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDeveloper.Model;
+
+namespace WebDeveloper.DataAccess
+{
+    class WebContextDb : DbContext //herencia
+    {
+        public WebContextDb() : base("name=WebDeveloperConnnectionString")
+        {
+                
+        }
+        public DbSet<Client> Clients { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
