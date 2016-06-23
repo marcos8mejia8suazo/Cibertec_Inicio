2783120393952a5d66bdb0d10de88d6e6cbceddd
marcos8mejia8suazo <marcos88mejia@hotmail.com>
2016-06-22 22:30:02 -0500

    RouteConfig

--- WebDeveloper/App_Start/RouteConfig.cs
+++ WebDeveloper/App_Start/RouteConfig.cs
@@ -14,9 +14,24 @@ namespace WebDeveloper
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Razor",
+                url: "Razor/{id}",
+                defaults: new
+                {
+                    controller = "Razor",
+                    action = "Product/Index",
+                    id = UrlParameter.Optional
+                }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new
+                {
+                    controller = "Home",
+                    action = "Index",
+                    id = UrlParameter.Optional }
             );
         }
     }
