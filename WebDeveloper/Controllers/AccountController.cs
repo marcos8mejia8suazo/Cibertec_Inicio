9a89f24bdac15d2c1b3a0593ce90a3ff406d1bed
marcos8mejia8suazo <marcos88mejia@hotmail.com>
2016-07-11 19:38:52 -0500

    LogOut

--- WebDeveloper/Controllers/AccountController.cs
+++ WebDeveloper/Controllers/AccountController.cs
@@ -66,6 +66,13 @@ namespace WebDeveloper.Controllers
             return View(model);
         }
 
+        [HttpPost]
+        public ActionResult LogOff()
+        {
+            AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+            return RedirectToAction("Index", "Home");
+        }
+
         private IAuthenticationManager AuthenticationManager
         {
             get
